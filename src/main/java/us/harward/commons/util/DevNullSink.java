d15e3ce6296608d64281f34304f56b03e3e4c033
Nathaniel Harward <nharward@gmail.com>
2010-11-01 05:51:31 +0000

    Screw it, making Guava Libraries a required dependency and merging to a single top-level project

---
+++ src/main/java/us/harward/commons/util/DevNullSink.java
@@ -0,0 +1,28 @@
+// Copyright 2010 Nathaniel Harward
+//
+// This file is part of ndh-commons.
+//
+// ndh-commons is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ndh-commons is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ndh-commons. If not, see <http://www.gnu.org/licenses/>.
+
+package us.harward.commons.util;
+
+/**
+ * You guessed it.
+ */
+public class DevNullSink<T> implements Sink<T> {
+
+    public void drop(final T t) {
+    };
+
+}
