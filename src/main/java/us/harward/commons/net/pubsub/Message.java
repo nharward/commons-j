762ccec378e38d6e10f486645b64c12a5a741724
Nathaniel Harward <nharward@gmail.com>
2011-07-26 12:40:05 -0700

    Re-factoring MessageBuilder to be an inner class 'Builder' of Message. Also adding source and server UUID fields in Message, this will be used later for multi-master functionality and could also be used for tracking/statistics.

--- src/main/java/us/harward/commons/net/pubsub/Message.java
+++ src/main/java/us/harward/commons/net/pubsub/Message.java
@@ -17,6 +17,8 @@
 
 package us.harward.commons.net.pubsub;
 
+import java.util.UUID;
+
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
 
@@ -49,16 +51,41 @@ abstract class Message {
 
     }
 
-    final Type type;
+    final Type   type;
+    private UUID sourceID;
+    private UUID serverID;
 
     protected Message(final Type type) {
         this.type = type;
+    }
+
+    void sourceID(final UUID sourceID) {
+        this.sourceID = sourceID;
+    }
+
+    UUID sourceID() {
+        return this.sourceID;
+    }
+
+    void serverID(final UUID sourceID) {
+        this.serverID = sourceID;
+    }
+
+    UUID serverID() {
+        return this.serverID;
     }
 
     final void marshall(final ChannelBuffer buffer) {
         final ChannelBuffer bodyBuffer = ChannelBuffers.dynamicBuffer(estimatedBodySize());
         marshallBody(bodyBuffer);
         buffer.writeBytes(type.bytes);
+        if (sourceID != null) {
+            buffer.writeLong(sourceID.getMostSignificantBits());
+            buffer.writeLong(sourceID.getLeastSignificantBits());
+        } else {
+            buffer.writeLong(0l);
+            buffer.writeLong(0l);
+        }
         buffer.writeInt(bodyBuffer.readableBytes());
         buffer.writeBytes(bodyBuffer);
     }
@@ -66,5 +93,68 @@ abstract class Message {
     abstract int estimatedBodySize();
 
     abstract void marshallBody(final ChannelBuffer buffer);
+
+    static final Builder newBuilder() {
+        return new Builder();
+    }
+
+    static final class Builder {
+
+        private final ChannelBuffer type;
+        private final ChannelBuffer sourceID;
+        private final ChannelBuffer serverID;
+        private final ChannelBuffer length;
+        private ChannelBuffer       body;
+
+        Builder() {
+            type = ChannelBuffers.buffer(4);
+            sourceID = ChannelBuffers.buffer(8);
+            serverID = ChannelBuffers.buffer(8);
+            length = ChannelBuffers.buffer(4);
+            body = null;
+        }
+
+        /**
+         * This message builder may be re-used from scratch immediately following when a non-null message is returned from this
+         * method. At such a point when a non-null return value is given here, all internal state is reset.
+         * 
+         * @param buffer
+         *            more data to add to this builder
+         * @return the message decoded by reading some or all of <code>buffer</code> or <code>null</code> if more data is needed
+         */
+        Message add(final ChannelBuffer buffer) throws MessageFormatException {
+            Preconditions.checkNotNull(buffer);
+            if (buffer.readable() && type.writable())
+                type.writeBytes(buffer, Math.min(buffer.readableBytes(), type.writableBytes()));
+            if (buffer.readable() && sourceID.writable())
+                sourceID.writeBytes(buffer, Math.min(buffer.readableBytes(), sourceID.writableBytes()));
+            if (buffer.readable() && serverID.writable())
+                serverID.writeBytes(buffer, Math.min(buffer.readableBytes(), serverID.writableBytes()));
+            if (buffer.readable() && length.writable())
+                length.writeBytes(buffer, Math.min(buffer.readableBytes(), length.writableBytes()));
+            if (!length.writable() && body == null)
+                body = ChannelBuffers.buffer(length.readInt());
+            if (body != null && buffer.readable() && body.writable())
+                body.writeBytes(buffer, Math.min(buffer.readableBytes(), body.writableBytes()));
+            final Message message;
+            if (body != null && !body.writable()) {
+                final Message.Type t = Message.Type.findByCode(type.toString(Charsets.UTF_8));
+                if (t == Message.Type.Application)
+                    message = new ApplicationMessage(body);
+                else if (t == Message.Type.Subscription)
+                    message = new SubscriptionMessage(body);
+                else
+                    throw new MessageFormatException("Unknown message type[" + type.toString(Charsets.UTF_8) + "]");
+                message.sourceID(new UUID(sourceID.readLong(), sourceID.readLong()));
+                message.serverID(new UUID(serverID.readLong(), serverID.readLong()));
+                type.clear();
+                length.clear();
+                body = null;
+            } else
+                message = null;
+            return message;
+        }
+
+    }
 
 }
