a6f09179f0c05c3c43cf908e416d295a23551189
Nathaniel Harward <nharward@gmail.com>
2013-03-16 23:59:35 -0700

    Correcting project name changes in copyrights, also adding missing copyrights

--- src/main/java/nerds/antelax/commons/io/ByteBufferInputStream.java
+++ src/main/java/nerds/antelax/commons/io/ByteBufferInputStream.java
@@ -14,6 +14,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with commons-j. If not, see <http://www.gnu.org/licenses/>.
+
 package nerds.antelax.commons.io;
 
 import java.io.IOException;
--- src/main/java/nerds/antelax/commons/xml/saxbp/annotations/SAXBPHandler.java
+++ src/main/java/nerds/antelax/commons/xml/saxbp/annotations/SAXBPHandler.java
@@ -1,19 +1,19 @@
 // Copyright 2010 Nathaniel Harward
 //
-// This file is part of ndh-commons.
+// This file is part of commons-j.
 //
-// ndh-commons is free software: you can redistribute it and/or modify
+// commons-j is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// ndh-commons is distributed in the hope that it will be useful,
+// commons-j is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with ndh-commons. If not, see <http://www.gnu.org/licenses/>.
+// along with commons-j. If not, see <http://www.gnu.org/licenses/>.
 
 package nerds.antelax.commons.xml.saxbp.annotations;
 
